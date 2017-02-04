ab77a6bc47b201d3fb767ef819c82760c90c01e1
Sean Vella <sean.j.vella@gmail.com>
2017-02-04 20:50:54 +0100

    Refresh automatically to gain access to article

--- assets/js/multi_user.js
+++ assets/js/multi_user.js
@@ -11,11 +11,32 @@ jQuery(document).ready(function($) {
 	  	})
 	}
 
+	function notifyUser(id, entry, ajaxURL){
+		setTimeout(
+			function() 
+			{
+			alert('You have not saved changes in 4 minutes. Save now or you will lose changes.')
+			}, 240000);
+
+		setTimeout(
+			function() 
+			{
+			//lose access to save
+			saveUser(0, entry, ajaxURL);
+			$('input[type="submit"], button[type="submit"]').attr('disabled','disabled')
+			alert('You have not saved changes in 5 minutes. The entry is now locked. Please refresh the page.')
+			}, 300000);
+	}
+	
+
 	//get info
 	id = parseInt($('#session li').eq(0).find('a').attr('href').match(/\/([^\/]+)\/?$/)[1]);
 	// name = $('#session li').eq(0).find('a').text();
 	entry = document.URL.match(/\/([^\/]+)\/?$/)[1];
 	ajaxURL = Symphony.Context.get('symphony') + '/extension/multi_user_edit/multi_user/';
+
+	//start the countdown
+	notifyUser(id, entry, ajaxURL);
 
 	//Check if the entry is being used by another author
 	$.ajax({
@@ -28,15 +49,32 @@ jQuery(document).ready(function($) {
 	    	response = $(response);
 	    	if(response.hasClass('locked')){
 	    		//if locked, get difference in minutes
-	    		$diff = response.find('#diff').text();
-	    		if($diff > 5){
+	    		diff = response.find('#diff').text();
+	    		if(diff > 5){
 	    			//Gain access to the article as author is taking too long.
 	    			console.log('Exceeded 5 minutes; Updated log.')
 	    			saveUser(id, entry, ajaxURL);
 	    		}
 	    		else{
-	    			console.log(response.find('.show').text());
-	    			console.log((5 - $diff).toFixed(2) + ' minute/s to go.');
+	    			alert(response.find('.show').text() +' '+ (5 - diff).toFixed(2) + ' minute/s to go. (Refreshing when timer is done)');
+	    			$('input[type="submit"], button[type="submit"]').attr('disabled','disabled');
+
+	    			newDiff = (5 - diff).toFixed(2) + " ";
+
+	    			arr = newDiff.split('.');
+	    		
+	    			var count = 0;
+	    			var seconds = (parseInt(arr[0] * 60) + parseInt(arr[1])) * 1000;
+
+					function timeout() {
+					    setTimeout(function () {
+					        count += 1;
+					        location.reload();
+					        // console.log('reload');
+					        timeout();
+					    }, seconds);
+					};
+					timeout();
 	    		}
 	    	}
 	    	else{
