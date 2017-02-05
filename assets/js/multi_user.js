10fe5039f3aced8a7bf1c43c4cb77efa27bc36de
Sean Vella <sean.j.vella@gmail.com>
2017-02-05 09:58:05 +0100

    Make timer use variable instead of hardcoding values

--- assets/js/multi_user.js
+++ assets/js/multi_user.js
@@ -11,12 +11,14 @@ jQuery(document).ready(function($) {
 	  	})
 	}
 
-	function notifyUser(id, entry, ajaxURL){
+	function notifyUser(id, entry, ajaxURL, seconds){
+		threeQuarters = ((seconds * 75) / 100);
+		full = seconds;
 		setTimeout(
 			function() 
 			{
-			alert('You have not saved changes in 4 minutes. Save now or you will lose changes.')
-			}, 240000);
+			alert('You have not saved changes yet. Save now or you will lose changes.')
+			}, threeQuarters);
 
 		setTimeout(
 			function() 
@@ -24,8 +26,8 @@ jQuery(document).ready(function($) {
 			//lose access to save
 			saveUser(0, entry, ajaxURL);
 			$('input[type="submit"], button[type="submit"]').attr('disabled','disabled')
-			alert('You have not saved changes in 5 minutes. The entry is now locked. Please refresh the page.')
-			}, 300000);
+			alert('You have not saved changes. The entry is now locked. Please refresh the page.')
+			}, full);
 	}
 	
 	//get info
@@ -62,27 +64,35 @@ jQuery(document).ready(function($) {
 	    	if(response.hasClass('locked')){
 	    		//if locked, get difference in minutes
 	    		diff = response.find('#diff').text();
+	    		user = response.find('.user').text();
+
+	    		//Calculate the difference in seconds.
+	    		newDiff = (configDiff - diff).toFixed(2) + " ";
+    			arr = newDiff.split('.');
+   
+    			var seconds = (parseInt(arr[0] * 60) + parseInt(arr[1])) * 1000;
+
 	    		if(diff > configDiff){
 	    			//Gain access to the article as other author is taking too long.
-	    			console.log('Exceeded 5 minutes; Updated entry.');
+	    			console.log('Exceeded '+configDiff+' minutes; Updated entry.');
 	    			//start the countdown
-					notifyUser(id, entry, ajaxURL);
+					notifyUser(id, entry, ajaxURL, (configDiff * 60));
 					//save a new entry in DB
 	    			saveUser(id, entry, ajaxURL);
 	    		}
+	    		else if(id == user){
+					//if locked and is the same user, do nothing
+					console.log('Granted access. Same User')
+					notifyUser(id, entry, ajaxURL, seconds);
+				}
 	    		else{
-	    			alert(response.find('.show').text() +' '+ (configDiff - diff).toFixed(2) + ' minute/s to go. (Refreshing when timer is done)');
+	    			alert(response.find('.show').text() +' '+ (configDiff - diff).toFixed(2) + ' minute/s to go.');
 	    			$('input[type="submit"], button[type="submit"]').attr('disabled','disabled');
-
-	    			newDiff = (configDiff - diff).toFixed(2) + " ";
-
-	    			arr = newDiff.split('.');
-	    		
-	    			var seconds = (parseInt(arr[0] * 60) + parseInt(arr[1])) * 1000;
 
 					function timeout() {
 					    setTimeout(function () {
-					        location.reload();
+					        // location.reload();
+					        alert('Refresh page to gain access.');
 					        timeout();
 					    }, seconds);
 					};
@@ -91,12 +101,36 @@ jQuery(document).ready(function($) {
 	    	}
 	    	else{
 		    	//Save User info
+		    	console.log('Entry was unlocked, locking it.')
 				saveUser(id, entry, ajaxURL);
 			}
 	    }
 	  }).done(function(response){
 	  	//code..
   	});
+
+	//when page closed, remove lock
+ //  	$( window ).unload(function() {
+	// 	$.ajax({
+	// 		method: 'GET',
+	// 		url: ajaxURL,
+	// 		data: {
+	// 		  entry: parseInt(entry)
+	// 		},
+	// 		success: function(response){
+	// 			response = $(response);
+	// 			if(response.hasClass('locked')){
+					
+	// 				user = response.find('.user').text();
+	// 				if(id == user){
+	// 					//if locked and is the same user, unlock it
+	// 					console.log('Removing')
+	// 					saveUser(0, entry, ajaxURL);
+	// 				}
+	// 			}
+	// 		}
+	// 	});
+	// });
 
 
 });
